c5a28f73632d33f4b2ed593c70b31260ba07865d
Piotr Jarnecki <piotr.jarnecki@gmail.com>
2023-10-29 19:49:30 +0100

    first commit

---
+++ src/main/java/User.java
@@ -0,0 +1,19 @@
+package course.hw;
+
+public class User {
+    public static void main(String[] args) {
+        if (args.length < 3) {
+            System.out.println("Usage: java UserInfoApp <firstName> <lastName> <age>");
+            return;
+        }
+
+        String firstName = args[0];
+        String lastName = args[1];
+        int age = Integer.parseInt(args[2]);
+
+        System.out.println("User Information:");
+        System.out.println("First Name: " + firstName);
+        System.out.println("Last Name: " + lastName);
+        System.out.println("Age: " + age);
+    }
+}
---
+++ src/main/java/course/hm/project/it/company/Employee.java
@@ -0,0 +1,2 @@
+package course.hm.project.it.company;public class Employee {
+}
