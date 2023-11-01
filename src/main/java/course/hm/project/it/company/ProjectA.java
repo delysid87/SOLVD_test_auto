af7c470b8a0efb22bcbb1974b9c4c313b852f820
Piotr Jarnecki <piotr.jarnecki@gmail.com>
2023-11-01 23:35:06 +0100

    ProjectA

--- src/main/java/course/hm/project/it/company/ProjectA.java
+++ src/main/java/course/hm/project/it/company/ProjectA.java
@@ -1,2 +1,28 @@
-package course.hm.project.it.company;public class ProjectA {
+package course.hm.project.it.company;
+
+public class ProjectA {
+    private String projectName;
+    private Technologies technologies;
+
+    public String getProjectName() {
+        return projectName;
+    }
+
+    public Technologies getTechnologies() {
+        return technologies;
+    }
+
+
+    public ProjectA(String projectName, Technologies technologies) {
+        this.projectName = projectName;
+        this.technologies = technologies;
+    }
+
+    @Override
+    public String toString() {
+        return "ProjectA{" +
+                "projectName='" + projectName + '\'' +
+                ", technologies='" + technologies.getDescription() + '\'' +
+                '}';
+    }
 }
